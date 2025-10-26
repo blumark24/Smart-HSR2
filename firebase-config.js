57120e93e521eb4ba7c0fcf9020db421f9ccead2
blumark24 <blumark24@gmail.com>
2025-10-26 18:55:51 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyC5HTAq-LeJn4GObn08REwKdqIeokKmwds",
   authDomain: "smart-hsr.firebaseapp.com",
   projectId: "smart-hsr",
-  storageBucket: "smart-hsr.firebasestorage.app",
+  storageBucket: "smart-hsr.appspot.com",  // ✅ تم تصحيحه هنا
   messagingSenderId: "885545362431",
   appId: "1:885545362431:web:2487096393f0fcbf29bdca",
   measurementId: "G-NH7J83MWT9"
